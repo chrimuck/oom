bf46ef28165c3ac44c09a5bbf0ba31b4c64a7334
chrimuck <christian.muckenhuber@gmx.at>
2016-05-10 18:02:38 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Mitarbeiter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task2
+{
+	public class Mitarbeiter
+	{
+		
+			// gehalt -> logischerweise private
+			private decimal gehalt;
+
+			// Vorname -> read only 
+			public String Vorname { get; }
+
+			// Nachname -> read + write (kann sich ändern)
+			public String Nachname { get; set;}
+
+			// Abteilung -> read + write (kann sich ändern)
+			public String Abteilung { get; set;}
+
+		public Mitarbeiter (string newVorname, string newNachname, string newAbteilung)
+			{
+
+			if (string.IsNullOrWhiteSpace(newVorname)) 
+				throw new ArgumentException("Vorname muss angegeben werden", nameof(newVorname));
+			if (string.IsNullOrWhiteSpace(newNachname)) 
+				throw new ArgumentException("Nachname muss angegeben werden", nameof(newNachname));
+			if (string.IsNullOrWhiteSpace(newAbteilung)) 
+				throw new ArgumentException("Abteilung muss angegeben werden", nameof(Abteilung));
+				Vorname = newVorname;
+				Nachname = newNachname;
+				Abteilung = newAbteilung;
+			}
+
+		public void AbteilungsWechsel (string newAbteilung)
+		{
+			Abteilung = newAbteilung;
+
+		}
+
+
+
+	}
+}
+
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var mitarbeiter = new[]
+			{
+				new Mitarbeiter("Herbert", "Prohaska", "Fussball"),
+				new Mitarbeiter("Werner", "Faymann", "Politik"),
+
+			};
+
+			foreach(var m in mitarbeiter)
+			{
+
+				Console.WriteLine($"Vorname={m.Vorname} | Nachname={m.Nachname} | Abteilung={m.Abteilung}");
+			}
+
+			mitarbeiter [1].AbteilungsWechsel ("Taxiunternehmen");
+
+			foreach(var m in mitarbeiter)
+			{
+
+				Console.WriteLine($"Vorname={m.Vorname} | Nachname={m.Nachname} | Abteilung={m.Abteilung}");
+			}
+		}
+
+
+	}
+}
